570e46b92bcaa81a67133794b334f6e1bbfb5d4c
Jisan Zaman <jzaman@wesleyan.edu>
2017-05-05 05:29:11 -0400

    fix url values

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const getCongressPerson = event => {
 	let name = $(elem).attr('name');
 	let district =  $(elem).attr('district-num');
 	let state = $(elem).attr('state');
-	const url = `https://congress.api.sunlightfoundation.com/legislators?state=${req.params.state}&district=${req.params.district}&apiKey=jzthegreat@gmail.com`;
+	const url = `https://congress.api.sunlightfoundation.com/legislators?state=${state}&district=${district}&apiKey=jzthegreat@gmail.com`;
 	$.get(url, data => {
 		console.log(data);
 	});
